e5c84e0117ab8d56ed0cf5369f5db59414ba1c73
Anne Chollet <anne.chollet@le-campus-numerique.fr>
2023-06-07 12:21:17 +0200

    exercice 2.3 creation de la BDD twitter 2.0

--- console16.sql
+++ console16.sql
@@ -57,4 +57,34 @@ CREATE TABLE hashtags_tweets(
                                 FOREIGN KEY (tweet_id) REFERENCES tweets (id) ON DELETE CASCADE
 );
 
+INSERT INTO users(username, email, address, postal_code, city, country)
+VALUES
+    ('Babar','babar@sql.fr','1 rue de SQL',74000, 'ANNECY','FRANCE'),
+    ('Dumbo','dumbo@sql.fr','2 rue de SQL',74000, 'ANNECY','FRANCE'),
+    ('Peter Pan','peterpan@sql.fr','3 rue de SQL',74000, 'ANNECY','FRANCE');
+
+--SELECT * FROM users;
+
+INSERT INTO tweets (content, createdAt, user_id)
+VALUES
+    ('tweet numero 1 Babar', '06-12-2022 06:30:59', 1),
+    ('tweet numero 2 Babar', '26-01-2023 15:15:30', 1),
+    ('tweet numero 3 Babar', '06-06-2023 11:50:20', 1),
+    ('tweet numero 1 Dumbo', '06-12-2022 06:30:59', 2),
+    ('tweet numero 2 Dumbo', '26-01-2023 15:15:30', 2),
+    ('tweet numero 3 Dumbo', '06-06-2023 11:50:20', 2),
+    ('tweet numero 1 Peter Pan', '06-12-2022 06:30:59', 3),
+    ('tweet numero 2 Peter Pan', '26-01-2023 15:15:30', 3),
+    ('tweet numero 3 Peter Pan', '06-06-2023 11:50:20', 3);
+
+--SELECT * FROM tweets;
+
+INSERT INTO tweets_users (user_id, tweet_id, created_at)
+VALUES
+    (1,4,'31-12-2022 23:59:50'),
+    (2,8,'31-05-2023 20:10:10'),
+    (3,1,'07-06-2023 03:15:05');
+
+SELECT * FROM tweets_users;
+
 
